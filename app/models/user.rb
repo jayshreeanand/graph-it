6e3ab57777379f9ff0afc74a98efcd60007b0175
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 12:09:37 +0530

    Typo fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
 
-  has_many :posts, dependent: :destroy
+  has_many :stories, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
