2aab749c932fdc53fda0b1a1aaf8f377c0bbebe6
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 18:16:35 +0530

    Setup bootstrap and jquery through webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("jquery")
 require("bootstrap-sprockets")
 require("dropzone")
 
+import 'bootstrap'
 import 'controllers'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
