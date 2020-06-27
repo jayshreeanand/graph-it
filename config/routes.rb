11f592f1c713ae96f944e87a1157afdfc66c3276
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 13:42:50 +0530

    Set up root pages for logged in / logged out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    root to: "devise/sessions#new"
+  end
 
-  root to: 'home#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
