a3fe4841898563517e69b886641ca2707a56c898
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 11:24:43 +0530

    Added fields for devise reconfirmable

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_06_27_053550) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
