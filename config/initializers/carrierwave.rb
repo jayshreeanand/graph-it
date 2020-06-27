5b07b624a0a7b0250f8261f47d323c79a16d8d59
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 12:28:13 +0530

    Setup carrierwave with AWS S3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,23 @@
+CarrierWave.configure do |config|
+  if Rails.env.production? || Rails.env.staging?
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      use_iam_profile: true,
+      persistent: false,
+      region: Rails.application.secrets.s3_region
+    }
+    config.fog_directory = Rails.application.secrets.s3_bucket
+    config.storage = :fog
+    config.remove_previously_stored_files_after_update = true
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  else
+    config.storage = :file
+    config.asset_host = ActionController::Base.asset_host
+  end
+
+  if Rails.env.test?
+    config.enable_processing = false
+  end
+end
