bd69c0c9c9ecf8a836cdcfe4b8f41ad26a0fe928
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 18:37:07 +0530

    Bootstrap + jquery works

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
-require("bootstrap-sprockets")
 require("dropzone")
 
 import 'bootstrap'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,8 @@
 const { environment } = require('@rails/webpacker')
 
-import { ProvidePlugin } from 'webpack'
-
 const webpack = require("webpack")
 
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
   Popper: ['popper.js', 'default']
