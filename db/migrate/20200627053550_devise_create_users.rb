821cfc9bbd566c4b686f3a8f2a4a87bc56e4dc56
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 13:51:16 +0530

    Added avatar to users

--- db/migrate/20200627053550_devise_create_users.rb
+++ db/migrate/20200627053550_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :avatar
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_06_27_062018) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.string "avatar"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
