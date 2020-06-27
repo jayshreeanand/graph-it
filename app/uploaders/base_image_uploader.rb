4b7a80a60565819c867ae288ee7ddf7e5fc3b734
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 14:49:29 +0530

    Whitelisted image extensions

--- app/uploaders/base_image_uploader.rb
+++ app/uploaders/base_image_uploader.rb
@@ -2,6 +2,10 @@
 class BaseImageUploader < BaseUploader
   include CarrierWave::MiniMagick
 
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
   # Create different versions of your uploaded files:
   version :thumb do
     process :resize_to_fit => [50, 50]
