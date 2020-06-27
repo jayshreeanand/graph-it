a47f19ceb853043a4836fba5bf7b505fb05f199c
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 20:08:13 +0530

    Dismissable alerts

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,5 @@
 class StoriesController < ApplicationController
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!
   before_action :set_story, only: [:show, :edit, :update, :destroy]
 
   def index
