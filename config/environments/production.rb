e6d9e97cf2307cb42ce34a6386eb14077a529e0b
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 15:16:18 +0530

    Setup action mailer  - Sendgrid for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,16 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { host: ENV['REMOTE_HOST'], port: ENV['REMOTE_PORT'].to_i }
 
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    authentication: :plain,
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    domain:         'heroku.com',
+    enable_starttls_auto: true
+  }
+
 end
