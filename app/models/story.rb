163767af78d87c4e200e17437e3364eed3ab5740
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-27 12:01:28 +0530

    User/Story relationships and validations

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,6 @@
 class Story < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
 
+  has_many :posts, dependent: :destroy
+
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
