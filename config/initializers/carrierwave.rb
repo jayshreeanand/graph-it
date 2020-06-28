17aadb8ccff0530110d2f1dc2f6b0420395642a6
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-28 20:58:17 +0530

    Display user friendly time on stories

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
-  if Rails.env.production? || Rails.env.staging? || Rails.env.development?
+  if Rails.env.production? || Rails.env.staging?
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
