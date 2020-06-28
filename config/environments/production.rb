3622a3c833b8089233b45848b6c62762250b1a85
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-28 22:34:18 +0530

    Updated action mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   
-  config.action_mailer.default_url_options = { host: ENV['REMOTE_HOST'], port: ENV['REMOTE_PORT'].to_i }
+  config.action_mailer.default_url_options = { host: ENV['REMOTE_HOST'] }
 
   
   config.action_mailer.delivery_method = :smtp
