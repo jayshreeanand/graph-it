da9898d8fbf76c4cc6dbf8b9c9aa593d9411356d
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-28 17:09:47 +0530

    Fixed dropzone dependency issues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require("dropzone")
 
 import 'bootstrap'
 import 'dropzone'
