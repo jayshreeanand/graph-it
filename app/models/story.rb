6b8aca498a3f9359f6729ba3ca7d2efb20fa2f1e
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-28 12:51:13 +0530

    Update attachment kind (video/audio) before save

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,9 +3,19 @@ class Story < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
+  validates :attachment, presence: true
 
   enum kind: { image: 0, video: 1 }
 
+  before_save :update_kind
 
   mount_uploader :attachment, StoryAttachmentUploader
+
+  private
+
+  def update_kind
+    if attachment.present?
+      self.kind = (attachment.content_type.include? 'video') ? 'video' : 'audio'
+    end
+  end
 end
