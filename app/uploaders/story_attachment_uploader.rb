8b390e3655eb45c00a373fa5d90cfeb56962c93a
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-28 21:01:50 +0530

    Image/video processing sizes

--- app/uploaders/story_attachment_uploader.rb
+++ app/uploaders/story_attachment_uploader.rb
@@ -13,11 +13,18 @@ class StoryAttachmentUploader < BaseUploader
   # end
 
   version :video, if: :video? do
-    # process :encode
-    process encode_video: [:mp4, resolution: "200x200"]
+    process encode_video: [:mp4, resolution: '854x480']
+  end
+
+  version :video_small, if: :video? do
+    process encode_video: [:mp4, resolution: '427x240']
   end
 
   version :image, if: :image? do
+    process :resize_to_fit => [1200, 600]
+  end
+
+  version :image_small, if: :image? do
     process :resize_to_fit => [600, 300]
   end
 
