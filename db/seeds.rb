1682a7152881c5bba8e7f9ab9126c3d77c1ad92d
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-29 23:14:58 +0530

    Generated specs and factories for stories controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.all.destroy_all
 
 u = User.new({
   first_name: 'Demo',
