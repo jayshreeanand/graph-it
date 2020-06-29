e42b54d13cd88a72b76a4f72d9b40de5c24ded9f
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-29 23:51:03 +0530

    Story - Controller tests

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -1,24 +1,29 @@
 require 'rails_helper'
 
-# Change this StoriesController to your project
 RSpec.describe StoriesController, type: :controller do
-
-    # This should return the minimal set of attributes required to create a valid
-    # Article. As you add validations to Article, be sure to adjust the attributes here as well.
-    login_user
-    
-    let(:valid_attributes) {
-        { title: 'Test title!', description: 'This is a test description' }
+  login_user
+  
+  let(:valid_attributes) {
+    { title: 'Test title!',
+      description: 'This is a test description',
+      attachment: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/files/story_image.jpg'))),
     }
+  }
 
-    let(:valid_session) { {} }
+  let(:valid_session) { {} }
 
-    describe 'GET #index' do
-        it 'returns a success response' do
-            Story.create! valid_attributes
-            get :index, params: {}, session: valid_session
-            expect(response).to be_successful # be_successful expects a HTTP Status code of 200
-            # expect(response).to have_http_status(302) # Expects a HTTP Status code of 302
-        end
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get :index, params: {}, session: valid_session
+      expect(response).to be_successful # be_successful expects a HTTP Status code of 200
     end
+  end
+
+  describe 'Create Story' do
+    it 'creates a story and returns a success response' do
+      Story.create! valid_attributes
+
+      expect(response).to be_successful
+    end
+  end
 end
--- spec/factories/devise.rb
+++ spec/factories/devise.rb
@@ -1,18 +1,9 @@
 FactoryBot.define do
   factory :user do
-    id { 1 }
-    email { 'demo@demo.com' }
+    sequence(:email) { |n| "email-#{n}@demo.com" }
     password { 'demo123' }
     confirmed_at { Time.now - 1.hour }
     first_name { 'Demo' }
     last_name { 'User' }
   end
-
-  # Not used in this tutorial, but left to show an example of different user types
-  # factory :admin do
-  #   id {2}
-  #   email {"test@admin.com"}
-  #   password {"qwerty"}
-  #   admin {true}
-  # end
 end
