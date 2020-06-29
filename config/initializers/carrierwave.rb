b838475312f51c689e10fb22399e51ecc7974426
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-06-29 13:44:05 +0530

    Fix video caching issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,13 +6,12 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      # persistent: false,
+      persistent: false,
       region: Rails.application.secrets.s3_region
     }
     config.fog_directory = Rails.application.secrets.s3_bucket
     config.storage = :fog
     config.remove_previously_stored_files_after_update = true
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   else
     config.storage = :file
     config.asset_host = ActionController::Base.asset_host
